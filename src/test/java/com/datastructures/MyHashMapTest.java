a0d3403f9d9c4bdd075fa2de645e9674e11138f4
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-20 18:02:26 +0530

    Added:Ability to find frequency of words in a large paragraph phrase

--- src/test/java/com/datastructures/MyHashMapTest.java
+++ src/test/java/com/datastructures/MyHashMapTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyHashMapTest {
+    //UC1
     @Test
     public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
         String sentence = "To be or not to be";
@@ -18,5 +19,23 @@ public class MyHashMapTest {
         int frequency = myHashMap.get("to");
         System.out.println(myHashMap);
         Assert.assertEquals(2,frequency);
+    }
+    //UC2
+
+    @Test
+    public void givenASentence_WhenWordsAreAddedToList_ShouldReturnParanoidFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+        MyLinkedHashMap<String,Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word : words){
+            Integer value = myLinkedHashMap.get(word);
+            if(value == null) value=1;
+            else value=value+1;
+            myLinkedHashMap.add(word,value);
+        }
+        int frequency = myLinkedHashMap.get("paranoid");
+        System.out.println(myLinkedHashMap);
+        Assert.assertEquals(3,frequency);
     }
 }
