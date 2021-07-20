8e4a07373be82efc818074531629c7da0967e6a3
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-20 18:23:57 +0530

    Added:Remove avoidable word from the phrase

--- src/test/java/com/datastructures/MyHashMapTest.java
+++ src/test/java/com/datastructures/MyHashMapTest.java
@@ -38,4 +38,24 @@ public class MyHashMapTest {
         System.out.println(myLinkedHashMap);
         Assert.assertEquals(3,frequency);
     }
+    
+    //UC3
+
+    @Test
+    public void givenASentence_WhenWordsAreAddedToList_ShouldBeAbleTo_RemoveGivenWordFromList() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+        MyLinkedHashMap<String,Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word : words){
+            Integer value = myLinkedHashMap.get(word);
+            if(value == null) value=1;
+            else value=value+1;
+            myLinkedHashMap.add(word,value);
+        }
+        System.out.println("Before Deletion:\n"+myLinkedHashMap);
+        INode removedNode = myLinkedHashMap.remove("avoidable");
+        System.out.println("After Deletion :\n"+myLinkedHashMap);
+        Assert.assertEquals("avoidable",removedNode.getKey());
+    }
 }
